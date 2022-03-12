06921fe96baffadc95a30dcab9747a8f167eb6c2
hugosenari <hugosenari@gmail.com>
2022-03-12 01:17:29 -0300

    chore: re index coco

---
+++ master/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = ''Code coverage CLI + library for Nim using LCOV'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-coco-master.flake = false;
+  inputs.src-coco-master.owner = "samuelroy";
+  inputs.src-coco-master.ref   = "refs/heads/master";
+  inputs.src-coco-master.repo  = "coco";
+  inputs.src-coco-master.type  = "github";
+  
+  inputs."cligen".dir   = "nimpkgs/c/cligen";
+  inputs."cligen".owner = "riinr";
+  inputs."cligen".ref   = "flake-pinning";
+  inputs."cligen".repo  = "flake-nimble";
+  inputs."cligen".type  = "github";
+  inputs."cligen".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."cligen".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."glob".dir   = "nimpkgs/g/glob";
+  inputs."glob".owner = "riinr";
+  inputs."glob".ref   = "flake-pinning";
+  inputs."glob".repo  = "flake-nimble";
+  inputs."glob".type  = "github";
+  inputs."glob".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."glob".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-coco-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-coco-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
